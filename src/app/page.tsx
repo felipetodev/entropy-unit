4782b43b6a89fe54b9650bb7957bcfb3aab1277f
felipetodev <fe.ossandon.u@gmail.com>
2023-04-22 01:41:28 -0400

    update styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,14 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className="flex h-screen flex-col items-center justify-between">
-
+    <main className="flex h-screen flex-col items-center justify-between overflow-hidden">
       <div className="h-screen items-center justify-center relative flex flex-col place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <h1 className='text-3xl font-semibold'>ENTROPY UNIT</h1>
-        <h3 className='my-2 text-sm'>COMING SOON</h3>
+        <h1 className='text-2xl sm:text-5xl lg:text-[60px] font-semibold tracking-wide'>
+          ENTROPY UNIT
+        </h1>
+        <h3 className='my-2 text-sm tracking-widest'>
+          COMING SOON
+        </h3>
       </div>
     </main>
   )
