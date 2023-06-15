6c1825498146669f0db4cd657f0360782afb64db
felipetodev <fe.ossandon.u@gmail.com>
2023-06-14 21:53:32 -0400

    add new font family

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,29 @@
 import './globals.css'
 import { Analytics } from '@vercel/analytics/react'
-import { Inter } from 'next/font/google'
+import { Bai_Jamjuree } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const baiJamjuree = Bai_Jamjuree({
+  subsets: ['latin'],
+  weight: ['200', '300', '400', '500', '600', '700'],
+  variable: '--font-jamjuree'
+})
 
 export const metadata = {
   title: 'Entropy Unit (soon)',
-  description: 'Entropy Unit Game Development',
+  description: 'Entropy Unit Game Development'
 }
 
-export default function RootLayout({
-  children,
+export default function RootLayout ({
+  children
 }: {
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang='es'>
       {process.env.NODE_ENV === 'production' && (
         <Analytics />
       )}
-      <body className={inter.className}>{children}</body>
+      <body className={baiJamjuree.className}>{children}</body>
     </html>
   )
 }
