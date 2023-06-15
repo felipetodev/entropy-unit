09df04fe58d49a9347afb37752641a6fd0d69723
felipetodev <fe.ossandon.u@gmail.com>
2023-06-14 21:53:00 -0400

    add entropy colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,16 +3,23 @@ module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
       },
-    },
+      colors: {
+        entropy: {
+          gray: '#6F7385',
+          neutral: '#F2F2F2',
+          red: '#C70039'
+        }
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 }
