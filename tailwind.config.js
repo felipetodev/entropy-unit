a1140e447ffc0a0deaaead6a427e52703266d249
felipetodev <fe.ossandon.u@gmail.com>
2023-10-24 20:31:17 -0300

    feat: add tw config

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        transducer: ['transducer-extended', 'sans-serif']
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
@@ -16,7 +19,59 @@ module.exports = {
         entropy: {
           gray: '#6F7385',
           neutral: '#F2F2F2',
-          red: '#C70039'
+          red: '#C70039',
+          grayUnit: '#F2F2F2',
+          black: '#121212',
+          slateGray: '#6F7385',
+          blueUnit: '#232329',
+          darkBlue: '#19191C'
+        },
+        border: 'hsl(var(--border))',
+        input: 'hsl(var(--input))',
+        ring: 'hsl(var(--ring))',
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+        primary: {
+          DEFAULT: 'hsl(var(--primary))',
+          foreground: 'hsl(var(--primary-foreground))'
+        },
+        secondary: {
+          DEFAULT: 'hsl(var(--secondary))',
+          foreground: 'hsl(var(--secondary-foreground))'
+        },
+        destructive: {
+          DEFAULT: 'hsl(var(--destructive))',
+          foreground: 'hsl(var(--destructive-foreground))'
+        },
+        muted: {
+          DEFAULT: 'hsl(var(--muted))',
+          foreground: 'hsl(var(--muted-foreground))'
+        },
+        accent: {
+          DEFAULT: 'hsl(var(--accent))',
+          foreground: 'hsl(var(--accent-foreground))'
+        },
+        popover: {
+          DEFAULT: 'hsl(var(--popover))',
+          foreground: 'hsl(var(--popover-foreground))'
+        },
+        card: {
+          DEFAULT: 'hsl(var(--card))',
+          foreground: 'hsl(var(--card-foreground))'
+        }
+      },
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)'
+      },
+      animation: {
+        'infinite-scroll': 'infinite-scroll 60s linear infinite',
+        'infinite-scroll-reverse': 'infinite-scroll 60s linear infinite reverse'
+      },
+      keyframes: {
+        'infinite-scroll': {
+          to: { transform: 'translate(calc(-50% - 2.5rem))' }
         }
       }
     }
