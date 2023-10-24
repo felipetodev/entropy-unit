bf9f3758da6bc4774b78aa341eb8c11092823e0c
felipetodev <fe.ossandon.u@gmail.com>
2023-10-24 20:07:52 -0300

    feat: add server actions flag

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false, // change,
+  experimental: {
+    serverActions: true
+  }
 }
 
 module.exports = nextConfig
