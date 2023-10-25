c24e4ff66f3dd470c582d08b43b4fa30176c6d01
felipetodev <fe.ossandon.u@gmail.com>
2023-10-24 23:02:11 -0300

    add constants file

---
+++ src/lib/constants.ts
@@ -0,0 +1,19 @@
+
+export const NAV_LINKS = [
+  {
+    href: '/',
+    label: 'HOME'
+  },
+  {
+    href: '/games',
+    label: 'JUEGOS'
+  },
+  {
+    href: '/team',
+    label: 'ESTUDIO'
+  },
+  {
+    href: '/contacto',
+    label: 'CONTACTO'
+  }
+]
