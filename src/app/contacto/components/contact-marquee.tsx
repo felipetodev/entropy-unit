c897b4504e4a6ff5174151be06f7b47fd3065c36
felipetodev <fe.ossandon.u@gmail.com>
2023-10-30 16:06:21 -0300

    fix: add mailto in marquee

--- src/app/contacto/components/contact-marquee.tsx
+++ src/app/contacto/components/contact-marquee.tsx
@@ -6,8 +6,10 @@ function ContactMarquee () {
     <Marquee className='bg-entropy-red' direction='right'>
       {Array.from({ length: 5 }).map((_, i) => (
         <li key={i} className='flex items-center py-2'>
-          <h4 className='text-4xl'>CONTACT@ENTROPYUNIT.COM</h4>
-          <EntropyArrow className='ml-20' />
+          <a href='mailto:hola@entropyunit.com' className='inline-flex items-center'>
+            <h4 className='text-4xl'>CONTACT@ENTROPYUNIT.COM</h4>
+            <EntropyArrow className='ml-20' />
+          </a>
         </li>
       ))}
     </Marquee>
