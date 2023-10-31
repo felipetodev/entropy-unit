402760b3d79c63f1e29efb60679cf9e78acc5e91
felipetodev <fe.ossandon.u@gmail.com>
2023-10-31 10:08:32 -0300

    fix: focus buttons navigation

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -4,7 +4,7 @@ import { cva, type VariantProps } from 'class-variance-authority'
 import { cn } from '@/lib/utils'
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center rounded-md text-[11px] font-semibold font-transducer transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
+  'inline-flex items-center justify-center rounded-md text-[11px] font-semibold font-transducer transition-colors focus-visible:outline-none focus-visible:text-entropy-grayUnit focus-visible:border-entropy-red focus-visible:bg-entropy-red disabled:pointer-events-none disabled:opacity-50',
   {
     variants: {
       variant: {
