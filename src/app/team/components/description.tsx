1571cc5bd49f472372e51888efc1371024bcdd54
felipetodev <fe.ossandon.u@gmail.com>
2023-10-31 11:25:47 -0300

    fix: block selection

--- src/app/team/components/description.tsx
+++ src/app/team/components/description.tsx
@@ -29,7 +29,7 @@ function Description () {
         </div>
       </section>
       <section className='px-5 sm:px-10 flex flex-col gap-y-[60px] sm:gap-y-[100px] pt-8 pb-24'>
-        <img className='block aspect-[5/9] w-full h-[60px]' src='eu-symbol.svg' alt='logo' />
+        <img className='mx-auto max-w-max block aspect-[5/9] w-full h-[60px]' src='eu-symbol.svg' alt='logo' />
         <p className='uppercase font-transducer max-w-4xl mx-auto text-xl text-entropy-slateGray font-semibold text-center'>
           En Entropy Unit, creemos en el poder de la sinergia entre el arte y la ingeniería. Nuestro objetivo es crear experiencias significativas que despierten la imaginación y dejen una huella duradera en el corazón de quienes las disfrutan.
         </p>
