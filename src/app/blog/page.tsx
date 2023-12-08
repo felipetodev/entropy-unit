a2dd0094c9f3b79cbdeff1b029e7e3a795a67fd6
felipetodev <fe.ossandon.u@gmail.com>
2023-12-08 00:33:38 -0300

    fix: blog bg gradient

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -5,7 +5,7 @@ import { blogCards } from '@/lib/constants'
 
 export default function Blog () {
   return (
-    <div className='min-h-screen bg-gradient-to-b from-[#121212] from-20% to-100% to-entropy-red px-5 sm:px-10 pb-[100px] pt-[170px]'>
+    <div className='min-h-screen bg-gradient-to-b from-[#121212] from-30% to-100% to-entropy-red px-5 sm:px-10 pb-[100px] pt-[170px]'>
       <h1 className='font-transducer text-xl sm:text-2xl md:text-4xl font-semibold mb-14 text-center'>
         Entropy Unit Dev Blog
       </h1>
